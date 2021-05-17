d191aadee018331121cf6941d7bc0bba8ae2043f
Sankalp <sankalpbanodia23@gmail.com>
2021-05-17 19:45:22 +0530

    activity done

--- ws.js
+++ ws.js
@@ -1,11 +1,21 @@
 const request = require('request');
-const fs=require('fs');
+//const fs=require('fs');
+const cheerio = require('cheerio');
 
-request("https://www.google.com", callback);
+request("https://www.espncricinfo.com/series/county-championship-2021-1244186/essex-vs-derbyshire-group-1-1244259/live-cricket-score", callback);
 
 
 function callback(error, response, html) {
     //console.log("body: ", body);
     //console.log(error);
-    if(!error) fs.writeFileSync("index.html", html)
+    //if(!error) fs.writeFileSync("index.html", html)
+
+    if(!error){
+
+
+        const manipulationtool = cheerio.load(html);
+        let comment = manipulationtool(".match-comment-wrapper .match-comment-long-text p");
+        let c = manipulationtool(comment[0]).text();
+        console.log(c);
+}
 }
