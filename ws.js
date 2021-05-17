2f975ba3a31c46f69caf95bde5a4e5f58271a950
Sankalp <sankalpbanodia23@gmail.com>
2021-05-17 18:48:21 +0530

    first commit

--- ws.js
+++ ws.js
@@ -1,9 +1,11 @@
 const request = require('request');
+const fs=require('fs');
 
 request("https://www.google.com", callback);
 
 
-function callback(error, response, body) {
-    console.log("body: ", body);
-    console.log(error);
+function callback(error, response, html) {
+    //console.log("body: ", body);
+    //console.log(error);
+    if(!error) fs.writeFileSync("index.html", html)
 }
